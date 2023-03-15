c6e2015f09101da1549a148ee219afedc5d713df
shivam1739 <patelshivam1739@gmail.com>
2023-03-15 16:28:36 +0530

    cors update

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ const commentRoute = require("./routes/comment.routes");
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 authRoute(app);
 roleRoute(app);
